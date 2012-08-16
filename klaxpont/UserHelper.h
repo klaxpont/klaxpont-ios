21ac3e20eb6c2338424ce17eb4fb46306218eb6b
François Benaiteau <francoisbenaiteau@iMac-de-Pierrot.local>
2012-08-16 18:48:00 +0200

    modifying user to user helper according to blog article

--- klaxpont/UserHelper.h
+++ klaxpont/UserHelper.h
@@ -1,15 +1,20 @@
 //
-//  User.h
+//  UserHelper.h
 //  klaxpont
 //
 //  Created by François Benaiteau on 4/10/12.
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+// using this helper user approach
+// see http://rogchap.com/2011/07/12/simple-user-helper-for-ios/
+
+
 #import <Foundation/Foundation.h>
 
-@interface User : NSObject
+@interface UserHelper : NSObject
 {
+    NSUserDefaults *_defaults;
 
     NSMutableData *_facebookPicture;
     NSString *_firstName;
@@ -18,10 +23,16 @@
     NSNumber *_klaxpontId;// Klaxpont User id
 }
 
+
 @property(nonatomic, retain, setter = setFirstName:) NSString* firstName;
 @property(nonatomic, retain, setter = setLastName:) NSString* lastName;
 @property(nonatomic, retain, setter = setFacebookPicture:) NSMutableData* facebookPicture;
 @property(nonatomic, retain, setter = setFacebookId:) NSNumber* facebookId;
 @property(nonatomic, retain, setter = setKlaxpontId:) NSNumber* klaxpontId;
+
++(UserHelper*) default;
+
 -(BOOL)isRegistered;
+-(BOOL)acceptedDisclaimer;
+-(void)acceptDisclaimer;
 @end
---
+++ klaxpont/UserHelper.m
@@ -0,0 +1,100 @@
+//
+//  UserHelper.m
+//  klaxpont
+//
+//  Created by François Benaiteau on 4/10/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "UserHelper.h"
+
+@implementation UserHelper
+
+@synthesize facebookPicture = _facebookPicture;
+@synthesize firstName = _firstName;
+@synthesize lastName = _lastName;
+@synthesize facebookId = _facebookId;
+@synthesize klaxpontId = _klaxpontId;
+
+
+
+static NSString *kcurrentUser = @"currentUser";
+
++(UserHelper *) default {
+    static UserHelper *helper = nil;
+    
+    if (helper == nil) {
+        helper = [[UserHelper alloc] init];
+    }
+    
+    return helper;
+}
+
+-(id)init
+{
+    if(self = [super init])
+    {
+        _defaults = [NSUserDefaults standardUserDefaults];
+        
+        _facebookPicture = [_defaults objectForKey:@"facebook_picture"];
+        _firstName = [_defaults objectForKey:@"facebook_first_name"];
+        _lastName = [_defaults objectForKey:@"facebook_last_name"];
+        _klaxpontId = [_defaults objectForKey:@"klaxpont_id"];
+    }
+    return self;
+}
+
+-(void)setKlaxpontId:(NSNumber*)klaxpontId
+{
+    _klaxpontId = klaxpontId;
+    [_defaults setObject:klaxpontId forKey:@"klaxpont_id"];
+    [_defaults synchronize];
+    
+}
+
+-(void)setFirstName:(NSString*)firstName
+{
+    _firstName = firstName;
+    [_defaults setObject:firstName forKey:@"facebook_first_name"];
+    [_defaults synchronize];
+    
+}
+
+-(void)setLastName:(NSString*)lastName
+{
+    _lastName = lastName;
+    [_defaults setObject:lastName forKey:@"facebook_last_name"];
+    [_defaults synchronize];
+
+}
+
+-(void)setFacebookPicture:(NSMutableData*)facebookPicture
+{
+    _facebookPicture = facebookPicture;
+    [_defaults setObject:facebookPicture forKey:@"facebook_picture"];
+    [_defaults synchronize];
+    
+}
+-(void)setFacebookId:(NSNumber*)facebookId
+{
+    _facebookId = facebookId;
+    [_defaults setObject:facebookId forKey:@"facebook_id"];
+    [_defaults synchronize];
+}
+
+-(BOOL)isRegistered
+{
+    return _klaxpontId != nil;
+}
+
+-(BOOL)acceptedDisclaimer
+{
+    return [_defaults boolForKey:@"accepted_disclaimer"];
+}
+
+-(void)acceptDisclaimer
+{
+    [_defaults setBool:YES forKey:@"accepted_disclaimer"];
+    [_defaults synchronize];
+}
+@end
