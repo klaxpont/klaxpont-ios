4b76c0e2cf24ffed06b618e8d87ea5cbdddc686a
François Benaiteau <francoisbenaiteau@imac-de-pierrot.tele2>
2012-08-17 15:23:05 +0200

    added methods for managed context to AppDelegate

--- klaxpont/AppDelegate.h
+++ klaxpont/AppDelegate.h
@@ -7,12 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+
+#import <CoreData/CoreData.h>
 #import <FacebookSDK/FacebookSDK.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 {
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 }
+
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
 
 @property (strong, nonatomic) FBSession *fbSession;
 @property (strong, nonatomic) UIWindow *window;
--- klaxpont/AppDelegate.m
+++ klaxpont/AppDelegate.m
@@ -15,6 +15,8 @@
 #import "TopVideosViewController.h"
 #import "AccountViewController.h"
 
+#import "Video.h"
+
 @implementation AppDelegate
 
 @synthesize window = _window;
@@ -122,4 +124,51 @@
 //}
 //
 //
+
+#pragma mark - Core Data
+
+- (NSManagedObjectContext *) managedObjectContext {
+    if (managedObjectContext != nil) {
+        return managedObjectContext;
+    }
+    NSPersistentStoreCoordinator *coordinator = [self persistentStoreCoordinator];
+    if (coordinator != nil) {
+        managedObjectContext = [[NSManagedObjectContext alloc] init];
+        [managedObjectContext setPersistentStoreCoordinator: coordinator];
+    }
+    
+    return managedObjectContext;
+}
+
+- (NSManagedObjectModel *)managedObjectModel {
+    if (managedObjectModel != nil) {
+        return managedObjectModel;
+    }
+    // used to retain this one, but we use ARC so just forget about it
+    managedObjectModel = [NSManagedObjectModel mergedModelFromBundles:nil];
+    
+    return managedObjectModel;
+}
+
+- (NSPersistentStoreCoordinator *)persistentStoreCoordinator {
+    if (persistentStoreCoordinator != nil) {
+        return persistentStoreCoordinator;
+    }
+    NSURL *storeUrl = [NSURL fileURLWithPath: [[self applicationDocumentsDirectory]
+                                               stringByAppendingPathComponent: @"klaxpont.sqlite"]];
+    NSError *error = nil;
+    persistentStoreCoordinator = [[NSPersistentStoreCoordinator alloc]
+                                  initWithManagedObjectModel:[self managedObjectModel]];
+    if(![persistentStoreCoordinator addPersistentStoreWithType:NSSQLiteStoreType
+                                                 configuration:nil URL:storeUrl options:nil error:&error]) {
+        /*Error for store creation should be handled in here*/
+    }
+    
+    return persistentStoreCoordinator;
+}
+
+- (NSString *)applicationDocumentsDirectory {
+    return [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject];
+}
+
 @end
