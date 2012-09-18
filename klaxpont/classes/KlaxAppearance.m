d740f432611f59659c23a3b81b79a7c5ebefda69
François Benaiteau <francois.benaiteau@gmail.com>
2012-09-19 00:40:57 +0200

    FIX appareance for uitabbar buttons

--- klaxpont/classes/KlaxAppearance.m
+++ klaxpont/classes/KlaxAppearance.m
@@ -16,18 +16,26 @@
     [[UINavigationBar appearance] setBarStyle:UIBarStyleBlack];
     [[UINavigationBar appearance] setTitleTextAttributes:@{UITextAttributeFont:[UIFont fontWithName:DEFAULT_FONT size:24]}];
 
-    
+    // General Buttons
     UILabel *labelAppearance = [UILabel appearance];
     [labelAppearance setFont:generalFont];
     [[UITextView appearance] setFont:generalFont];
     [labelAppearance setTextColor:[UIColor blackColor]];
 
-    
-    [[UILabel appearanceWhenContainedIn:[UITabBar class], nil]
-     setFont:[UIFont fontWithName:DEFAULT_FONT size:14]];
-
+   
     UIButton *buttonAppearance = [UIButton appearance];
     [buttonAppearance setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+
+    // TabBar Buttons
+    [[UIButton appearanceWhenContainedIn:[UITabBar class], nil]
+     setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [[UILabel appearanceWhenContainedIn:[UITabBar class], nil]
+     setTextColor:[UIColor whiteColor]];
+    [[UILabel appearanceWhenContainedIn:[UITabBar class], nil]
+     setFont:[UIFont fontWithName:DEFAULT_FONT size:14]];
+    
+
+
     
 }
 
